72463f9b6f7beb5ba4996065cadd482f0c931235
Oakley Jurgens <o.jurgens@gns.cri.nz>
2025-08-29 10:08:16 +1200

    [16] shared workflow for API deployment

--- .github/actions/apiSmokeTest/src/index.js
+++ .github/actions/apiSmokeTest/src/index.js
@@ -1,8 +1,9 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
 import fs from "node:fs";
 
+// find the first URL
 const urlRegex = /https:.*/
+// Assume that the APIKey's name begins with TempAPIKey and then take the string after that name
 const keyRegex = /(?<=TempApiKey.* )[^ ]*/
 
 function hasValue(candidate) {
@@ -13,27 +14,34 @@ function hasValue(candidate) {
     }
 }
 
-try {
-
+async function smokeTest() {
+    // get inputs
     const query = core.getInput("query");
     const workingDir = core.getInput("working-directory");
     const altUrl = hasValue(core.getInput("url"));
     const expectedRegex = new RegExp(core.getInput("expected-regex"));
 
-    // // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context, undefined, 2);
-    // core.info(payload)
-
+    // open deploy.out and extract API URL and API key
     const path = !workingDir || workingDir == '.' || workingDir == '' ? '' : '/' + workingDir;
-
     const data = fs.readFileSync(process.cwd() + path + '/deploy.out', 'utf8');
     const url = altUrl || data.match(urlRegex)[0];
     const key = data.match(keyRegex)[0];
+
+    if (!hasValue(url)) {
+        core.setFailed("Could not extract API URL from deployment output");
+        return;
+    }
+
+    if (!hasValue(key)) {
+        core.setFailed("Could not extract API key from deployment output");
+        return;
+    }
 
     core.info("URL: " + url);
     core.info("Key: " + key);
     core.info("Query: " + query);
 
+    // send sample query to the API
     const response = await fetch(url, {
         method: "POST",
         headers: {
@@ -44,17 +52,23 @@ try {
     })
 
     if (!response.ok) {
-        core.setFailed(`Smoke test response: ${response.status}`);
+        core.setFailed(`Smoke test response: ${response.status}, ${await response.text()}`);
+        return;
     }
     const result = await response.text()
     core.info("Response: " + result)
 
+    // chenk if response matches the expected regex
     if (result.match(expectedRegex)) {
         core.info("Matches: " + expectedRegex);
     } else {
         core.error("Does not match " + expectedRegex);
         core.setFailed("Smoke test does not return expected result");
     }
+}
+
+try {
+    await smokeTest();
 } catch (error) {
     core.setFailed("Smoke test failed: " + error.message);
 }
